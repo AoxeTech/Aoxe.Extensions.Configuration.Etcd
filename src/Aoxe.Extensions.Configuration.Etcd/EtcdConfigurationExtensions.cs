31c5faffd1586d61a419c314ffbbb91116cb706d
RomanDu <aeondxf@live.com>
2024-09-09 09:38:17 +0800

    Dev

--- src/Aoxe.Extensions.Configuration.Etcd/EtcdConfigurationExtensions.cs
+++ src/Aoxe.Extensions.Configuration.Etcd/EtcdConfigurationExtensions.cs
@@ -4,8 +4,15 @@ public static class EtcdConfigurationExtensions
 {
     public static IConfigurationBuilder AddEtcd(
         this IConfigurationBuilder builder,
-        EtcdClientOptions etcdClientOptions,
+        Func<EtcdClientOptions> optionsFactory,
         string key,
         IFlattener? flattener = null
-    ) => builder.Add(new EtcdConfigurationSource(etcdClientOptions, key, flattener));
+    ) => builder.Add(new EtcdConfigurationSource(optionsFactory, key, flattener));
+
+    public static IConfigurationBuilder AddEtcd(
+        this IConfigurationBuilder builder,
+        EtcdClientOptions options,
+        string key,
+        IFlattener? flattener = null
+    ) => builder.Add(new EtcdConfigurationSource(options, key, flattener));
 }
--- src/Aoxe.Extensions.Configuration.Etcd/EtcdConfigurationSource.cs
+++ src/Aoxe.Extensions.Configuration.Etcd/EtcdConfigurationSource.cs
@@ -1,13 +1,20 @@
 namespace Aoxe.Extensions.Configuration.Etcd;
 
 public class EtcdConfigurationSource(
-    EtcdClientOptions etcdClientOptions,
+    EtcdClientOptions options,
     string key,
     IFlattener? flattener = null
 ) : IConfigurationSource
 {
-    public EtcdClientOptions EtcdClientOptions { get; } = etcdClientOptions;
+    public EtcdClientOptions EtcdClientOptions { get; } = options;
     public string Key { get; } = key;
+
+    public EtcdConfigurationSource(
+        Func<EtcdClientOptions> optionsFactory,
+        string key,
+        IFlattener? flattener = null
+    )
+        : this(optionsFactory(), key, flattener) { }
 
     public IConfigurationProvider Build(IConfigurationBuilder builder) =>
         new EtcdConfigurationProvider(this, new EtcdClientFactory(this), flattener);
